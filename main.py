0b105fe9b402c2e260fdf695b5b4edc975d65d86
Tevin Rose <tevjason@hotmail.com>
2021-06-19 15:09:02 +0000

    Added more quotes

--- main.py
+++ main.py
@@ -11,21 +11,32 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "upset"]
 
 starter_encouragements = [
   "Don't give up you got this",
   "Keep pushing!",
   "Feel better soon!",
   "You're a beast!",
-  "You got this champ"
+  "You got this fam",
+  "Estas bien"
 ]
 
 depressed_quotes = [
-  "The glaciers are melting!", 
+  "Corona Virus", 
   "We're all gonna die",
   "Taxes and death are what's sure in this life",
-  "You lose some and you lose some more :|"
+  "You lose some and you lose some more :|",
+  "Y'all smoke to enjoy it. I smoke to die.",
+  "I used to sell furniture for a living, the trouble was, it was my own",
+  "There’s nothing wrong with you that an expensive operation can’t prolong.",
+  "There is no sunrise so beautiful that it is worth waking me up to see it.",
+  "I’m not insane. My mother had me tested.",
+  "I’m at a place in my life when errands are starting to count as going out.",
+  "To call you stupid would be an insult to stupid people!",
+  "Accept who you are. Unless you’re a serial killer.",
+  "I prefer not to think before speaking. I like being as surprised as everyone else by what comes out of my mouth.",
+  "Woke up today. It was terrible."
 
 ]
 
@@ -57,7 +68,8 @@ def delete_encouragement(index):
       del encouragements[index]
       db["encouragements"] = encouragements
    
-      
+
+    
 
 @client.event
 async def on_ready():
@@ -85,6 +97,7 @@ async def on_message(message):
     options = starter_encouragements
     if "encouragements" in db.keys():
      options.extend(db["encouragements"][:])
+    
        
      
 
@@ -96,6 +109,11 @@ async def on_message(message):
       update_encouragements(encouraging_message)
       await message.channel.send("New encouraging message added.")
 
+  #if msg.startswith("!newDep"):
+      #depressing_message = msg.split("!newDep ", 1)[1]
+      #update_depressing(depressing_message)
+      #await message.channel.send("New depressing message added.")
+
   if msg.startswith("!del"):
     encouragements = []
     if "encouragements" in db.keys():
@@ -104,11 +122,31 @@ async def on_message(message):
       encouragements = db["encouragements"]
     await message.channel.send(encouragements)
 
+  #if msg.startswith("!delDep"):
+    #depressing = []
+    #if "depressing" in db.keys():
+      #index = int(msg.split("!delDep", 1)[1])
+      #delete_encouragement(index)
+      #depressing = db["depressing"]
+    #await message.channel.send(encouragements)
+
+
   if msg.startswith("!list"):
     encouragements = []
+    depressing = []
     if "encouragements" in db.keys():
      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+     await message.channel.send(encouragements)
+    if "depressing" in db.keys():
+     depressing = db["depressing"]
+     await message.channel.send(depressing)
+    
+
+  #if msg.startswith("!listDep"):
+   # depressing = []
+    #if "depressing" in db.keys():
+     #depressing = db["depressing"]
+    #await message.channel.send(depressing)
 
   if msg.startswith("!responding"):
    value = msg.split("!responding ",1)[1]
